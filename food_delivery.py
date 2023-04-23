04ac9998ad4ac37df83c53bc268e2d7e815dfa59
YugandarReddy <yugandarreddynarra@gmail.com>
2023-04-23 18:27:56 -0500

    login and register pages working

--- food_delivery.py
+++ food_delivery.py
@@ -1,69 +1,78 @@
+import uuid
 from flask import Flask,render_template,request, redirect, url_for,session
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 from datetime import datetime
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-'''
-conn = sqlite3.connect('food_delivery.db')
-c = conn.cursor()
-'''
-'''
-class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(200), nullable=False)
-    phone = db.Column(db.String(200), nullable=False)
-    street = db.Column(db.String(200), nullable=False)
-    city = db.Column(db.String(200), nullable=False)
-    state = db.Column(db.String(200), nullable=False)
-    pincode = db.Column(db.Integer, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
 
-    def __repr(self):
-        return '<Customer %r>' % self.name
-'''
-'''
+app.secret_key='123456789'
+conn = sqlite3.connect('food_delivery.db',check_same_thread=False)
+c = conn.cursor()
+
 RESTAURANT=[{"RestaurantID":1,"Name":"Biryani House","DeliveryFee":5,"City":"Wichita","State":"Ks","PinCode":123455},{"RestaurantID":2,"Name":"Dosa Palace","DeliveryFee":3,"City":"Wichita","State":"Ks","PinCode":123455},{"RestaurantID":3,"Name":"Paradise","DeliveryFee":6,"City":"Wichita","State":"Ks","PinCode":123455},{"RestaurantID":4,"Name":"Bawarchi","DeliveryFee":3,"City":"Wichita","State":"Ks","PinCode":123457}]
 MENU=[{"ItemID":1,"Name":"Biryani","Price":15,"Preparation_Time":20,"Description":"Rice Flavoured with Indian Spices"},{"ItemID":2,"Name":"Idli","Price":10,"Preparation_Time":10,"Description":"Steam cooked pan cake batter with dip of tomato chutney"},{"ItemID":3,"Name":"Dosa","Price":5,"Preparation_Time":7,"Description":"pan cake batter with dip of coconut chutney"},{"ItemID":4,"Name":"Tandoori Chicken","Price":15,"Preparation_Time":20,"Description":"Chicken roasted in Tandoor"},{"ItemID":5,"Name":"Grill Chicken","Price":15,"Preparation_Time":20,"Description":"Chicken Grilled on Charcoal"}]
-'''
 
+c.execute('''CREATE TABLE IF NOT EXISTS customers (CustomerId INTEGER PRIMARY KEY AUTOINCREMENT, CustomerName VARCHAR(100),Phone VARCHAR(10),Street VARCHAR(50),City VARHCAR(50),State VARCHAR(50),Pincode INTEGER,Email VARCHAR(50),Password VARCHAR(15))''')
+c.execute('''CREATE TABLE IF NOT EXISTS restaurants (RestaurantId INTEGER PRIMARY KEY AUTOINCREMENT, RestaurantName VARCHAR(100),Phone VARCHAR(10),Street VARCHAR(50),City VARHCAR(50),State VARCHAR(50),Pincode INTEGER,Email VARCHAR(50),Password VARCHAR(15))''')
+conn.commit()
+c.execute('''INSERT INTO customers VALUES (NULL,'Yugi','1234567890','2330 N','Wichita','Kansas',67220,'123@gmail.com','1234')''')
+conn.commit()
 #Login Page
 @app.route("/")
 @app.route("/login",methods=['POST','GET'])
 def login():
     if request.method=='POST' and 'username' in request.form and 'password' in request.form:
+        user_type = request.form['user_type']
+        print(user_type)
         phone = request.form['username']
         password = request.form['password']
-    return render_template('login.html')
-    '''
-        Connect database to check for user
-        c.execute("SELECT * FROM accounts WHERE phone = % s AND password = % s", (phone, password))'
+        #Connect database to check for user
+        if user_type == 'Customer':
+            c.execute("SELECT * FROM customers WHERE phone = ? AND password = ?", (phone, password))
+        elif user_type == 'Restaurant':            
+            c.execute("SELECT * FROM restaurants WHERE phone = ? AND password = ?", (phone, password))
+        else:
+            return render_template('login.html',"Incorrect User Type")
         account = c.fetchone()
         conn.commit()
-        
         if account:
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            msg = 'Logged in successfully !'
-            return render_template('index.html', msg = msg)
+            return render_template('restaurant_list.html',restaurants=RESTAURANT)
         else:
-            msg = 'Incorrect username / password !'
-'''
+            return render_template('login.html',msg= "Incorrect Phone Number/Password")
+    return render_template('login.html')
 
+#Register Page
 @app.route('/register', methods =['GET', 'POST'])
 def register():
-    if request.method=='POST' :
-         return render_template('login.html')   
+    if request.method=='POST' and 'user_type' in request.form and 'name' in request.form and 'phone' in request.form and 'street' in request.form and 'city' in request.form and 'state' in request.form and 'pincode' in request.form and 'email' in request.form and 'password' in request.form and 'confirm_password' in request.form:     
+        user_type=request.form['user_type']
+        name = request.form['name']
+        phone = request.form['phone']
+        street = request.form['street']
+        city = request.form['city']
+        state = request.form['state']
+        pincode = request.form['pincode']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password == confirm_password:
+            if user_type == 'Customer':
+                c.execute("INSERT INTO customers VALUES (NULL,?,?,?,?,?,?,?,?)",(name,phone,street,city,state,pincode,email,password))
+            elif user_type == 'Restaurant':
+                c.execute("INSERT INTO restaurants VALUES (NULL,?,?,?,?,?,?,?,?)",(name,phone,street,city,state,pincode,email,password))
+        else:
+            return render_template('register.html',msg="Both Passwords do not match")
+        conn.commit()
+        return render_template('login.html')
     return render_template('register.html')
 
+#Home Page
 @app.route('/restaurant_list',methods=['GET','POST'])
 def restaurant_list():
     if request.method=='POST' :
          return render_template('login.html')   
-    return render_template('restaurant_list.html')
+    return render_template('restaurant_list.html',restaurants=RESTAURANT)
 
 
 if __name__== '__main__':
